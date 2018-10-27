21e776d4b204f3deded8218fc6e0f07619138b6c
Akshit Sehgal <akshitsehgal95@gmail.com>
2018-10-27 23:22:39 +0530

    Numbers

---
+++ 1.Basics/2.Numbers.js
@@ -0,0 +1,17 @@
+let num = 34;
+let sub = num - -9;
+let sum = num + 9;
+let mult = num * 9;
+let div = num / 9;
+console.log(num, sub, sum, mult, div);
+
+let result1 = num + 1 * 2;
+let result2 = (num + 1) * 2;
+console.log(result1, result2);
+
+// Challenge
+
+let score = 56;
+let maxScore = 112;
+let percent = (score / maxScore) * 100;
+console.log('Percentage:', percent,'%');
