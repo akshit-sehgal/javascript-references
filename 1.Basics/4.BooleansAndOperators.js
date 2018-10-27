8c70385456b41f6f73b803d0d8255afc2fa9c011
Akshit Sehgal <akshitsehgal95@gmail.com>
2018-10-27 23:48:28 +0530

    Booleans and Comparison Operators

---
+++ 1.Basics/4.BooleansAndOperators.js
@@ -0,0 +1,19 @@
+// === equality operator
+// !== not equal to operator
+let temp = 31;
+let isFreezing = temp === 31;
+console.log(isFreezing);
+
+// < less than operator
+// > greater than operator
+// <= less than equal to operator
+// >= greater than equal to operator
+let result = temp <= 32;
+console.log(result);
+
+// Challenge
+let age = 100;
+let isChild = age <= 7;
+let isSenior = age >= 65;
+console.log('isChild: ', isChild);
+console.log('isSenior: ', isSenior);
