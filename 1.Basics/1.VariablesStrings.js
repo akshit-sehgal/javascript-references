57afe77c664ab86c18b29068a0b522ccff38f693
Akshit Sehgal <akshitsehgal95@gmail.com>
2018-10-27 23:10:30 +0530

    Variables and strings

---
+++ 1.Basics/1.VariablesStrings.js
@@ -0,0 +1,11 @@
+let firstName = 'Bruce';
+let lastName = 'Lee';
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// Challenge
+
+let city = 'New Delhi';
+let country = 'India';
+let location = city + ', ' + country;
+console.log(location);
