d847b16ed0b3aee948d58baa9ca2c163028b63de
Akshit Sehgal <akshitsehgal95@gmail.com>
2018-10-27 23:36:08 +0530

    More on variables and mini temperature converter project

---
+++ 1.Basics/3.MoreOnVariables.js
@@ -0,0 +1,11 @@
+// 1. We can`t define a variable more than once
+let petName = 'Puff';
+petName = 'Spark';
+console.log(petName);
+
+// 2. Thre are rules related to variable names
+// Variable names can start with _, $, A - Z, a - z
+// Variable names cannot be reserved keywords
+let a1 = 45;
+let $one = 1;
+let _one = 1;
---
+++ 1.Basics/P1.TemperatureConverter.js
@@ -0,0 +1,8 @@
+let fahrenheit = 50;
+let celsius = (fahrenheit - 32) * 5 / 9;
+let kelvin = celsius + 273.15;
+
+console.log('\nTemperature Converter:');
+console.log(fahrenheit + ' F');
+console.log(celsius + ' C');
+console.log(kelvin + ' K');
